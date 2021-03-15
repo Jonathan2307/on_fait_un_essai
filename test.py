5ed1df4b67c1f65ec3ec37392248365e59cda897
Sebastien Le Digabel <sledigabel@gmail.com>
2021-03-15 21:50:30 +0000

    ajout de la barre de progres

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 #!/bin/env python3
 
+import time
+import sys
 
 if __name__ == '__main__':
     print("hello!")
+
+    for i in range(48):
+        sys.stdout.write('#'*i+'\r')
+        sys.stdout.flush()
+        time.sleep(1)
+    print()
